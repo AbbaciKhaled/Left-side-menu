37321da440bc987e5f735ae7d8f176721b8b49fe
abbacikhaled <abbacikhaled@gmail.com>
2021-10-09 15:32:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+function openMenu() {
+    document.getElementById("menu").className != "close-menu" ? document.getElementById("menu").className = "close-menu" : document.getElementById("menu").className = "open-menu"
+}
